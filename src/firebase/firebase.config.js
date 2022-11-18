1450702466ed5a9197461c702b1dab4bdae92510
Al Amin Eimon <eimonalamin@gmail.com>
2022-11-18 10:26:18 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBWzET0YjSfl-7HrZVZ3agl348GBaVm-C0",
+  authDomain: "aircnc-87283.firebaseapp.com",
+  projectId: "aircnc-87283",
+  storageBucket: "aircnc-87283.appspot.com",
+  messagingSenderId: "160281530830",
+  appId: "1:160281530830:web:39404a130be97d5bec9b9f",
+};
+
+const app = initializeApp(firebaseConfig)
+
+export default app
